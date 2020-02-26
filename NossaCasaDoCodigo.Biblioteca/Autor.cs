9717cf7ae7d5b6f5a0c50761f8bdbd86f2786bbf
Fabiano Teodoro <faateodoro@gmail.com>
2020-02-26 11:32:07 -0300

    Finalizando Autores

---
+++ NossaCasaDoCodigo.Biblioteca/Autor.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace NossaCasaDoCodigo.Biblioteca
+{
+    public class Autor
+    {
+        private string _nome;
+
+        public string Nome
+        {
+            get { return _nome; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("O nome deve ser informado.");
+                else
+                    _nome = value; 
+            }
+        }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            private set
+            {
+                _email = ValidaEmail(value);
+            }
+        }
+
+        private string _descricao;
+
+        public string Descricao
+        {
+            get { return _descricao; }
+            private set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("A descrição não pode ser vazia.");
+                else if (value.Length > 400)
+                    throw new ArgumentException("A descrição não pode ter mais que 400 caracteres.");
+                else
+                    _descricao = value; 
+            }
+        }
+
+        public string DataCadastro { get; }
+
+        public Autor(string nome, string email, string descricao)
+        {
+            Nome = nome;
+            Email = email;
+            Descricao = descricao;
+
+            DataCadastro = DateTime.Now.ToString();
+        }
+
+        private string ValidaEmail(string email)
+        {
+            Regex expressao = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+
+            if (!expressao.IsMatch(email))
+                throw new ArgumentException("O formato do e-mail é inválido.");
+            else
+                return email;
+        }
+    }
+}
---
+++ NossaCasaDoCodigo.Testes/TestaAutor.cs
@@ -0,0 +1,60 @@
+﻿using NossaCasaDoCodigo.Biblioteca;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NossaCasaDoCodigo.Testes
+{
+    public class TestaAutor
+    {
+        [Theory]
+        [InlineData("", "fabiano@gmail.com", "Um cara iniciando na carrera de TI.")]
+        [InlineData(null, "fabiano@gmail.com", "Um cara iniciando na carrera de TI.")]
+        public void CampoNomeNaoPodeSerVazioOuNulo(string nome, string email, string descricao)
+        {
+            Assert.Throws<ArgumentException>(() => new Autor(nome, email, descricao));
+        }
+
+        [Theory]
+        [InlineData("Fabiano", "fabiano", "Um cara iniciando na carrera de TI.")]
+        [InlineData("Fabiano", "fabiano@gmail", "Um cara iniciando na carrera de TI.")]
+        [InlineData("Fabiano", "fabianogmail.com", "Um cara iniciando na carrera de TI.")]
+        [InlineData("Fabiano", "", "Um cara iniciando na carrera de TI.")]
+        public void CampoEmailDevePossuirFormatoValido(string nome, string email, string descricao)
+        {
+            Assert.Throws<ArgumentException>(() => new Autor(nome, email, descricao));
+        }
+
+        [Fact]
+        public void CampoEmailnaoPodeSerNulo()
+        {
+            Assert.Throws<ArgumentNullException>(() => new Autor("Fabiano", null, "Um cara iniciando na carrera de TI."));
+        }
+
+
+        [Theory]
+        [InlineData("Fabiano", "fabiano@gmail.com", "")]
+        [InlineData("Fabiano", "fabiano@gmail.com", null)]
+        [InlineData("Fabiano", "fabiano@gmail.com",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. " +
+            "Aliquam non condimentum tellus. Nulla vulputate enim et sodales vehicula. " +
+            "Aenean vel scelerisque augue. Pellentesque luctus ornare tincidunt. " +
+            "Vivamus ultrices est massa, quis luctus ex posuere et. " +
+            "Duis commodo urna sed velit accumsan volutpat. " +
+            "Vivamus eu nisl in orci mollis efficitur id at ex. Vivamus vel molestie urna. " +
+            "Ut fringilla feugiat lacinia. Morbi cras amet.")]
+        public void CampoDescricaoNaoPodeSerVazioOuNuloComMenosDeQuatrocentrosCaracteres
+            (string nome, string email, string descricao)
+        {
+            Assert.Throws<ArgumentException>(() => new Autor(nome, email, descricao));
+        }
+
+        [Fact]
+        public void MomentoDoCadastroEstaIncluso()
+        {
+            var autor = new Autor("Fabiano", "fabiano@gmail.com", "Um cara iniciando na carrera de TI.");
+            Assert.False(string.IsNullOrEmpty(autor.DataCadastro));
+        }
+    }
+}
