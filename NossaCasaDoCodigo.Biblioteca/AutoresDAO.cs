2b0dc76a096cb98341e4d99bad33a1afcc4924c3
Fabiano Teodoro <faateodoro@gmail.com>
2020-04-08 17:27:34 -0300

    Adicionando AutoresDAO

---
+++ NossaCasaDoCodigo.Biblioteca/AutoresDAO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NossaCasaDoCodigo.Biblioteca
+{
+    public class AutoresDAO
+    {
+        public AutoresDAO()
+        {
+        }
+
+        public static IList<Autor> Autores { get; set; }
+
+        public static bool SalvaAutor(Autor novoAutor)
+        {
+            if (Autores == null)
+            {
+                Autores = new List<Autor>();
+            }
+
+            if (Autores.Count > 0)
+            {
+                foreach (var autor in Autores)
+                {
+                    if (novoAutor.Email == autor.Email)
+                    {
+                        throw new ArgumentException("Email já existente!");
+                    }
+                }
+            }
+
+            Autores.Add(new Autor(novoAutor.Nome, novoAutor.Email, novoAutor.Descricao));
+            return true;
+        }
+    }
+}
